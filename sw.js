d752e1bc2b6f0561a6a1aa7c8a1811c2461691ed
leonardomaranesismm-maker <leonardomaranesismm@gmail.com>
2025-10-10 16:23:00 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,21 +9,6 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
-      .then(() => self.skipWaiting())
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    }).then(() => self.clients.claim())
   );
 });
 
